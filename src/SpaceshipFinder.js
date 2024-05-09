07a1c8f2dc05d281c615e305579044e3850309da
Ron <if22b080@technikum-wien.at>
2024-05-09 16:38:59 +0200

    Refactor test 2

--- src/SpaceshipFinder.js
+++ src/SpaceshipFinder.js
@@ -1,7 +1,11 @@
 function SpaceshipFinder() {}
 SpaceshipFinder.prototype.search = function(map) {
-    if (!(map && map.includes('X'))) { // wenn map oder schiff nicht vorhanden
+    if (cannotFindShip(map)) { // wenn map oder schiff nicht vorhanden
         return "Spaceship lost forever.";
     }
     return;
 };
+
+function cannotFindShip(map){
+    return !(map && map.includes('X'));
+}
