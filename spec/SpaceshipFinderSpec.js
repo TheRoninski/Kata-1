107864338ed966781ed28b33c63cef6f6fa303db
Ron <if22b080@technikum-wien.at>
2024-05-09 16:27:38 +0200

    Red failed test 2

--- spec/SpaceshipFinderSpec.js
+++ spec/SpaceshipFinderSpec.js
@@ -1,9 +1,13 @@
-describe('Spaceship Finder', function() {
+describe('SpaceshipFinder', function() {
 	beforeEach(function() {
 		findSpaceship = new SpaceshipFinder();
 	});
 
 	it('should return "Spaceship lost forever." for an empty map', function() {
 		expect(findSpaceship.search('')).toEqual("Spaceship lost forever.");
+	});
+
+	it('should return "Spaceship lost forever." for null input', function() {
+		expect(findSpaceship.search('....')).toEqual("Spaceship lost forever.");
 	});
 });
